14efe572f1b011abd5949276f62306e3afd1a34c
Anjan-Kumar-Sahoo <anjankumarsahoo1097@gmail.com>
2025-07-01 10:06:30 +0530

    Changes in models.py

--- core/models.py
+++ core/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-    upi_id = models.CharField(max_length=50,unique=True)
+    upi_id = models.CharField(max_length=50, unique=True)
     full_name = models.CharField(max_length=100)
-    creadted_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.full_name} ({self.upi_id})"
-    
+
 class Pool(models.Model):
     name = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,10 +15,10 @@ class Pool(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 class PoolMember(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    pool = models.ForeignKey(Pool,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pool = models.ForeignKey(Pool, on_delete=models.CASCADE)
     joined_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
