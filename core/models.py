9dac5ee91274f688faf2390acd7240e077f9c868
Anjan-Kumar-Sahoo <anjankumarsahoo1097@gmail.com>
2025-07-01 10:19:58 +0530

    Step 4 :Added Bill & BillSplit models

--- core/models.py
+++ core/models.py
@@ -23,3 +23,23 @@ class PoolMember(models.Model):
 
     class Meta:
         unique_together = ('user', 'pool')
+
+class Bill(models.Model):
+    pool = models.ForeignKey(Pool, on_delete=models.CASCADE)
+    paid_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - ₹{self.amount} in {self.pool.name}"
+
+class BillSplit(models.Model):
+    bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
+    owed_by = models.ForeignKey(User, related_name='owes', on_delete=models.CASCADE)
+    owed_to = models.ForeignKey(User, related_name='gets', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    is_settled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.owed_by} owes {self.owed_to} ₹{self.amount}"
